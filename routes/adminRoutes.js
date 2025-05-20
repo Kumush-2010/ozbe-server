afd4e0cd4121da4e88587e0a45fb527c6c748401
Kumush <168472075+Kumush-2010@users.noreply.github.com>
2025-05-20 15:37:29 +0500

    Add files via upload

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,16 +1,40 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { authMiddleware, adminMiddleware } = require('../middleware/auth');
-const { getAdminStats } = require('../controllers/adminController.js');
+const { authMiddleware, adminMiddleware } = require("../middleware/auth");
+const { getAdminStats } = require("../controllers/adminpageController.js");
 
 // 👑 Admin statistikasi
-router.get('/stats', authMiddleware, adminMiddleware, getAdminStats);
+router.get("/stats", authMiddleware, adminMiddleware, getAdminStats);
 
-router.post('/admins', authMiddleware, allowRoles('superadmin'), createAdmin);
-router.put('/admins/:id', authMiddleware, allowRoles('superadmin'), updateAdmin);
-router.delete('/admins/:id', authMiddleware, allowRoles('superadmin'), deleteAdmin);
-router.get('/admins', authMiddleware, allowRoles('admin', 'superadmin'), getAllAdmins);
-router.put('/admins/profile', authMiddleware, allowRoles('admin', 'superadmin'), updateOwnProfile);
-
+router.post(
+  "/adminpages",
+  authMiddleware,
+  allowRoles("superadmin"),
+  createAdmin
+);
+router.put(
+  "/adminpages/:id",
+  authMiddleware,
+  allowRoles("superadmin"),
+  updateAdmin
+);
+router.delete(
+  "/adminpages/:id",
+  authMiddleware,
+  allowRoles("superadmin"),
+  deleteAdmin
+);
+router.get(
+  "/adminpages",
+  authMiddleware,
+  allowRoles("admin", "superadmin"),
+  getAllAdmins
+);
+router.put(
+  "/adminpages/profile",
+  authMiddleware,
+  allowRoles("admin", "superadmin"),
+  updateOwnProfile
+);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,8 +1,53 @@
-const app = require("./app");
+const express = require("express");
+const Handlebars = require("handlebars");
+const {
+  allowInsecurePrototypeAccess,
+} = require("@handlebars/allow-prototype-access");
+const { create } = require("express-handlebars");
+const path = require("path");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const flash = require("connect-flash");
+const session = require("express-session");
 
+const app = express();
+
+// Handlebars sozlamalari
+const hbs = create({
+  defaultLayout: "main", // layout fayli: main.hbs
+  extname: "hbs",
+  handlebars: allowInsecurePrototypeAccess(Handlebars),
+});
+
+// app.engine("hbs", hbs.engine);
+// app.set("view engine", "hbs");
+// app.set("views", path.join(__dirname, "adminpage")); // sahifalar shu yerda
+
+app.engine("hbs", hbs.engine);
+app.set("view engine", "hbs");
+app.set("views", "./adminpage/views");
+
+// Middlewares
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use(cookieParser());
+app.use(session({ secret: "Admin", resave: false, saveUninitialized: false }));
+app.use(flash());
+
+app.use(express.static("adminpage"));
+
+// Routes
+app.get("/", (req, res) => {
+  res.redirect("/admin");
+});
+
+app.get("/admin", (req, res) => {
+  return res.render("dashboard", { title: "Admin Panel", layout: false }); // main.hbs layout ichida views/dashboard.hbs yuklanadi
+});
+
+// Port
 const PORT = process.env.PORT || 5000;
-
 app.listen(PORT, () => {
   console.log(`🚀 Server ${PORT}-portda ishlamoqda`);
 });
-
