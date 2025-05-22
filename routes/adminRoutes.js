d33653321c2b9c97592e1003780a1510bfeb1cf7
Kumush <168472075+Kumush-2010@users.noreply.github.com>
2025-05-22 17:43:59 +0500

    Add files via upload

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,40 +1,55 @@
 const express = require("express");
 const router = express.Router();
-const { authMiddleware, adminMiddleware } = require("../middleware/auth");
-const { getAdminStats } = require("../controllers/adminpageController.js");
+// const { authMiddleware, adminMiddleware } = require("../middleware/auth");
+// const { getAdminStats } = require("../controllers/adminController.js");
+const { adminCreate } = require('../controllers/adminController.js');
 
-// 👑 Admin statistikasi
-router.get("/stats", authMiddleware, adminMiddleware, getAdminStats);
+router.get('/create', (req, res) => {
+  res.render('admin-create'); // hbs fayl nomi
+});
 
-router.post(
-  "/adminpages",
-  authMiddleware,
-  allowRoles("superadmin"),
-  createAdmin
-);
-router.put(
-  "/adminpages/:id",
-  authMiddleware,
-  allowRoles("superadmin"),
-  updateAdmin
-);
-router.delete(
-  "/adminpages/:id",
-  authMiddleware,
-  allowRoles("superadmin"),
-  deleteAdmin
-);
-router.get(
-  "/adminpages",
-  authMiddleware,
-  allowRoles("admin", "superadmin"),
-  getAllAdmins
-);
-router.put(
-  "/adminpages/profile",
-  authMiddleware,
-  allowRoles("admin", "superadmin"),
-  updateOwnProfile
-);
+router.post('/create', adminCreate);
 
 module.exports = router;
+
+//"adminname": "admin",
+  // "birth": "2010-01-01",
+  // "jins": "ayol",
+  // "phone": "+998904385114",
+  // "email": "kumush@gmail.com",
+  // "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSBnBg4GGhbqK6YYf95JDTBnoVf0rhF3en3GsXXnXWMkqFxR8_j5cw4LEgfOVKJT6siwdY&usqp=CAU",
+  // "password": "kumush12" 
+
+// 👑 Admin statistikasi
+// router.get("/stats", authMiddleware, adminMiddleware, getAdminStats);
+
+// router.post(
+//   "/adminpages",
+//   authMiddleware,
+//   allowRoles("superadmin"),
+//   createAdmin
+// );
+// router.put(
+//   "/adminpages/:id",
+//   authMiddleware,
+//   allowRoles("superadmin"),
+//   updateAdmin
+// );
+// router.delete(
+//   "/adminpages/:id",
+//   authMiddleware,
+//   allowRoles("superadmin"),
+//   deleteAdmin
+// );
+// router.get(
+//   "/adminpages",
+//   authMiddleware,
+//   allowRoles("admin", "superadmin"),
+//   getAllAdmins
+// );
+// router.put(
+//   "/adminpages/profile",
+//   authMiddleware,
+//   allowRoles("admin", "superadmin"),
+//   updateOwnProfile
+// );
